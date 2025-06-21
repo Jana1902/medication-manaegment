a64f286da0890f95785ffacf1a076ac96ff0b20c
Jana1902 <jana7cr@gmail.com>
2025-06-21 20:18:03 +0530

    Updated Readme

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -1,7 +1,23 @@
-import { Navigate } from "react-router-dom";
+// import { Navigate } from "react-router-dom";
+
+// const ProtectedRoute = ({ children }) => {
+//   const token = localStorage.getItem("jwtToken");
+
+//   if (!token) {
+//     return <Navigate to="/login" replace />;
+//   }
+
+//   return children;
+// };
+
+// export default ProtectedRoute;
+
+
+// components/ProtectedRoute/index.js
+import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("jwtToken");
+  const token = localStorage.getItem('jwtToken');
 
   if (!token) {
     return <Navigate to="/login" replace />;
